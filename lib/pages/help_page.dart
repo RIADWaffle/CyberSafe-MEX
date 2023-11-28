0b643be2fa91e2da81c935c65eba802c9ac7c702
Roberto <i_criss@live.com.mx>
2023-11-27 18:48:04 -0600

    Added pdf viewer

--- lib/pages/help_page.dart
+++ lib/pages/help_page.dart
@@ -1,16 +1,20 @@
 // ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class HelpPage extends StatelessWidget {
+class HelpPage extends StatefulWidget {
   const HelpPage({super.key});
 
   @override
+  State<HelpPage> createState() => _HelpPageState();
+}
+
+class _HelpPageState extends State<HelpPage> {
+  String path = "lib/assets/test.pdf";
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("ayuda"),
-      ),
-    );
+    return Scaffold(body: SfPdfViewer.asset(path));
   }
 }
